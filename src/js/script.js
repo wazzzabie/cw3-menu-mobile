f8d0052bbb13c25dc9cfdd58aff4dea99b8c215f
Rudy Breton <cool-rudy-cool@hotmail.com>
2020-10-06 23:34:30 -0400

    Menu-Mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event)  {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
+
+    var hamburgers = document.querySelectorAll('.hamburger');
+    var menuMobile = document.querySelector('.nav-primary-mobile');
+
+    for(let i = 0 ; i < hamburgers.length; i++){
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener('click',openMenu);
+    }
+
+    function openMenu(evt){
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+
+        if(cible.classList.contains('open')){
+            cible.classList.remove('open');
+            menuMobile.classList.remove('open');
+        }else{
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+
+    }
+
 
 });
